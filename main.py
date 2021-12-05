1d3f154a480228b118b1277be012185c10f7144b
HP_Elitebook1040\1771 <takahashi.nx7.1013@gmail.com>
2021-12-05 16:32:37 +0900

    stat.print

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import psutil
 
-
 while True:
     mem_us = psutil.virtual_memory()
     cpu_us = psutil.cpu_percent(interval=1)
-    print(mem_us.percent)
-    print(cpu_us)
+    print("RAM_Use = " + str(mem_us.percent) + "%")
+    print("CPU_Use = " + str(cpu_us) + "%")
