dbb036474e8cf9c6e0238930d8a6fafee70082fd
HP_Elitebook1040\1771 <takahashi.nx7.1013@gmail.com>
2021-12-05 23:33:06 +0900

    print usarage 1 line

--- main.py
+++ main.py
@@ -3,5 +3,8 @@ import psutil
 while True:
     mem_us = psutil.virtual_memory()
     cpu_us = psutil.cpu_percent(interval=1)
-    print("RAM_Use = " + str(mem_us.percent) + "%")
-    print("CPU_Use = " + str(cpu_us) + "%")
+    print("\rRAM_Use = " + str(mem_us.percent) + "%", flush=True)
+    print("CPU_Use = " + str(cpu_us) + "%" + "\033[1A", end='', flush=True)
+
+    #print(type(mem_us))
+    #print(type(cpu_us))
